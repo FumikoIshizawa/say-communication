9fdfe7227001c12477dc9d801ffa4e2ae36e1f4d
Yu Hayakawa <s1520679@u.tsukuba.ac.jp>
2015-09-04 14:41:46 +0900

    メニューのJP用配列

--- say.sh
+++ say.sh
@@ -12,14 +12,18 @@ if test `date +"%H"` -eq `echo $TIME_GOHAN|cut -d":" -f1`;
     then
       #メニューの配列をインポート
       source ./menu_array
+      #ごはんの乱数
+      menu_array_size=`expr ${#menu_array[*]} - 1`;
+      menu_random_num=`jot -r 1 0 $menu_array_size`
+
       #言わせる言葉
-      KOTOBA="gohan no z kkun death, ${menu_array[$menu_random_num]} tabeyo"
+#      KOTOBA="gohan no z kkun death, ${menu_array[$menu_random_num]} tabeyo"
       KOTOBA1="gohan no z kkun death,"
       KOTOBA2="${menu_array[$menu_random_num]}"
       KOTOBA3="tabeyo"
       osascript -e 'set Volume 4'
       say -v $SPEAKER $KOTOBA1
-      osascript -e 'set Volume 6'
+      osascript -e 'set Volume 5'
       say -v $SPEAKER $KOTOBA2
       osascript -e 'set Volume 4'
       say -v $SPEAKER $KOTOBA3
